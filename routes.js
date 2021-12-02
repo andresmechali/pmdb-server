316796cd9979db7a3cd444e18b52cb3b3a35a64a
simobouz9898 <59710652+simobouz9898@users.noreply.github.com>
2021-12-02 10:20:23 +0100

    Add files via upload

--- routes.js
+++ routes.js
@@ -23,39 +23,40 @@ async function Person(req, res) {
     SELECT DISTINCT P.person_id, M.movie_id AS highest_grossing_IsCast_movie_id, primary_title AS highest_grossing_IsCast_title, lifetime_grossing AS highest_grossing_IsCast_grossing
     FROM Persons P JOIN IsCast IC on P.person_id = IC.person_id
                 JOIN Movies M on IC.movie_id = M.movie_id
-    WHERE P.person_id = '${person_id}'
+    WHERE P.person_id = ${person_id}
     ORDER BY lifetime_grossing DESC#
     LIMIT 1
-  ), highest_grossing_IsWriter AS (
-      SELECT DISTINCT P.person_id, M.movie_id AS highest_grossing_IsWriter_movie_id, primary_title AS highest_grossing_IsWriter_title, lifetime_grossing AS highest_grossing_IsWriter_grossing
-      FROM Persons P JOIN IsWriter IW on P.person_id = IW.person_id
-                  JOIN Movies M on IW.movie_id = M.movie_id
-      WHERE P.person_id = '${person_id}'
-      ORDER BY lifetime_grossing DESC
-      LIMIT 1
-  ), highest_grossing_IsDirector AS (
-      SELECT DISTINCT P.person_id, M.movie_id AS highest_grossing_IsDirector_movie_id, primary_title AS highest_grossing_IsDirector_title, lifetime_grossing AS highest_grossing_IsDirector_grossing
-      FROM Persons P JOIN IsDirector ID on P.person_id = ID.person_id
-                  JOIN Movies M on ID.movie_id = M.movie_id
-      WHERE P.person_id = '${person_id}'
-      ORDER BY lifetime_grossing DESC
-      LIMIT 1
-  ), person_and_highest_grossign_information AS (
-      SELECT P.person_id, primary_name, birth_year, death_year,
-            highest_grossing_IsCast_movie_id, highest_grossing_IsCast_title, highest_grossing_IsCast_grossing,
-            highest_grossing_IsWriter_movie_id, highest_grossing_IsWriter_title, highest_grossing_IsWriter_grossing,
-            highest_grossing_IsDirector_movie_id, highest_grossing_IsDirector_title, highest_grossing_IsDirector_grossing
-      FROM Persons P JOIN highest_grossing_IsCast IC ON IC.person_id = P.person_id
-                      JOIN highest_grossing_IsDirector ID ON ID.person_id = P.person_id
-                      JOIN highest_grossing_IsWriter IW ON IW.person_id = P.person_id
-  )
+), highest_grossing_IsWriter AS (
+  SELECT DISTINCT P.person_id, M.movie_id AS highest_grossing_IsWriter_movie_id, primary_title AS highest_grossing_IsWriter_title, lifetime_grossing AS highest_grossing_IsWriter_grossing
+  FROM Persons P JOIN IsWriter IW on P.person_id = IW.person_id
+              JOIN Movies M on IW.movie_id = M.movie_id
+  WHERE P.person_id = ${person_id}
+  ORDER BY lifetime_grossing DESC
+  LIMIT 1
+), highest_grossing_IsDirector AS (
+  SELECT DISTINCT P.person_id, M.movie_id AS highest_grossing_IsDirector_movie_id, primary_title AS highest_grossing_IsDirector_title, lifetime_grossing AS highest_grossing_IsDirector_grossing
+  FROM Persons P JOIN IsDirector ID on P.person_id = ID.person_id
+              JOIN Movies M on ID.movie_id = M.movie_id
+  WHERE P.person_id = ${person_id}
+  ORDER BY lifetime_grossing DESC
+  LIMIT 1
+), person_and_highest_grossign_information AS (
   SELECT P.person_id, primary_name, birth_year, death_year,
-          highest_grossing_IsCast_movie_id, highest_grossing_IsCast_title, highest_grossing_IsCast_grossing,
-          highest_grossing_IsWriter_movie_id, highest_grossing_IsWriter_title, highest_grossing_IsWriter_grossing,
-          highest_grossing_IsDirector_movie_id, highest_grossing_IsDirector_title, highest_grossing_IsDirector_grossing,
-          M2.movie_id AS movie_id_title_known_for, primary_title AS title_known_for
-  FROM person_and_highest_grossign_information P JOIN IsKnownFor IKF ON P.person_id = IKF.person_id
-      JOIN Movies M2 on IKF.movie_id = M2.movie_id
+        highest_grossing_IsCast_movie_id, highest_grossing_IsCast_title, highest_grossing_IsCast_grossing,
+        highest_grossing_IsWriter_movie_id, highest_grossing_IsWriter_title, highest_grossing_IsWriter_grossing,
+        highest_grossing_IsDirector_movie_id, highest_grossing_IsDirector_title, highest_grossing_IsDirector_grossing
+  FROM Persons P LEFT JOIN highest_grossing_IsCast IC ON IC.person_id = P.person_id
+                  LEFT JOIN highest_grossing_IsDirector ID ON ID.person_id = P.person_id
+                  LEFT JOIN highest_grossing_IsWriter IW ON IW.person_id = P.person_id
+  WHERE P.person_id = ${person_id}
+)
+SELECT P.person_id, primary_name, birth_year, death_year,
+      highest_grossing_IsCast_movie_id, highest_grossing_IsCast_title, highest_grossing_IsCast_grossing,
+      highest_grossing_IsWriter_movie_id, highest_grossing_IsWriter_title, highest_grossing_IsWriter_grossing,
+      highest_grossing_IsDirector_movie_id, highest_grossing_IsDirector_title, highest_grossing_IsDirector_grossing,
+      M2.movie_id AS movie_id_title_known_for, GROUP_CONCAT( distinct primary_title) AS title_known_for
+FROM person_and_highest_grossign_information P JOIN IsKnownFor IKF ON P.person_id = IKF.person_id
+  JOIN Movies M2 on IKF.movie_id = M2.movie_id;
   `;
 
   const responseHandler = (error, results) => {
